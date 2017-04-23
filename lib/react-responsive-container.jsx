9a69bc02b969715b1057b007abe5cd6686828275
Diegoni <romain_diegoni@hotmail.com>
2017-04-23 17:49:05 +0100

    Develop panel positioned on the right

--- lib/react-responsive-container.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './react-responsive-container.scss';
-
-
-export default React.createClass({
-  render: function() {
-    return <div className="react-responsive-container">Hello World</div>;
-  }
-});
